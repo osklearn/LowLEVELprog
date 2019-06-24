fd7368e921b53855fad1a91f65125478c88cb0bb
dalexach <dalexach@gmail.com>
2019-06-24 19:20:05 +0000

    _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+* main - Print Holberton
+*Return: 0 (success)
+*/
+int main(void)
+{
+	int i;
+	char n[9] = "Holberton";
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(n[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
