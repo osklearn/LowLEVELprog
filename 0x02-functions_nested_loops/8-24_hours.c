2472bf44582db5923a184a80168d125673305d01
dalexach <dalexach@gmail.com>
2019-06-24 21:30:39 +0000

    Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+* jack_baurer - print every minute of the day of Jack Bauer
+*
+*Return: nothing
+*/
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar(':');
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
