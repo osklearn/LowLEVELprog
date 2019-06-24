fdb4c23dc558dd0696f08f2efd8750e6d7352b4e
dalexach <dalexach@gmail.com>
2019-06-24 19:55:02 +0000

    Absolute of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+* _abs - takes a number into a absolute
+* @n: number reciebed
+*
+*Return: 1 a letter 0 otherwise
+*/
+int _abs(int n)
+{
+	return (abs(n));
+}
