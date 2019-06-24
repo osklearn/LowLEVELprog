3af80f9d283c0aee16cb3596a191bb7b6784a03b
dalexach <dalexach@gmail.com>
2019-06-24 21:10:18 +0000

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+* print_last_digit - takes the last digit of a number
+* @n: number recieved
+*
+*Return: 1 a letter 0 otherwise
+*/
+int print_last_digit(int r)
+{
+	int last;
+	last = abs(r%10);
+	_putchar(last + '0');
+	return (last);
+}
