be49da7de7667df3ba6ac7f3b8f86110c412d04a
dalexach <dalexach@gmail.com>
2019-06-24 19:42:07 +0000

    Sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* print_sign - check the number inserted
+* @n: number reciebed
+*
+*Return: 1 greater zero  0 is zero -1 less zero
+*/
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
