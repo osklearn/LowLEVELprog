11438e340cd4175d85e74436df2e3215ebaca6e7
dalexach <dalexach@gmail.com>
2019-06-25 14:51:46 +0000

    Checking alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <ctype.h>
 /**
 * _isalpha - chack for aplphabetic character
 * @c: character recieved
@@ -9,7 +8,9 @@
 
 int _isalpha(int c)
 {
-	if (isalpha(c) != 0)
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
