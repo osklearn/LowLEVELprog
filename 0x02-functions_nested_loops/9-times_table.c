5a8cb54d7d003465fbecafebd0862d6fcdfecb90
dalexach <dalexach@gmail.com>
2019-06-25 15:15:07 +0000

    Table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+/**
+* times_table - ptints the 9 times table starting with 0
+*
+*Return: nothing
+*/
+void times_table(void)
+{
+	int r,c;
+	int d,u;
+	int mul;
+
+	for (r = 0; r <= 9; r++)
+	{
+		for (c = 0; c <= 9; c ++)
+		{
+			mul = r * c;
+			if (mul >= 10)
+			{
+				d = mul / 10;
+				u = mul % 10;
+				_putchar(' ');
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+				if (c != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(mul + '0');
+			}
+			if (c != 9)
+				_putchar(',');
+			if (c == 9)
+				_putchar('\n');
+		}
+	}	
+}
