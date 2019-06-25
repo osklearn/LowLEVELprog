f2c68820fb794dede76015e7a0f8a84b7c9f5075
dalexach <dalexach@gmail.com>
2019-06-25 15:55:52 +0000

    Printing from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* print_to_98 - print all numbers from n to 98
+* @n: number recieved
+*
+*Return: nothing
+*/
+void print_to_98(int n)
+{
+	int i;
+	if (n > 98)
+	{
+		for(i = n; i >= 98; i--)
+		{
+			printf("%d, ",i);
+		}
+		printf("\n");
+	}
+	else if (n < 0 && n<98)
+	{
+		for(i = n; i <= 98; i++)
+		{
+			printf("%d, ",i);
+		} 
+		printf("\n");
+	}
+	else
+	{
+		for(i = n; i <= 98; i++)
+		{
+			printf("%d, ",i);
+		}
+		printf("\n");
+	}
+}
