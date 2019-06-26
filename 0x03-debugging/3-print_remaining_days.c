15217e5836e4208a430bafaadcc5d81e745c9da0
dalexach <dalexach@gmail.com>
2019-06-26 04:30:25 +0000

    Debbug of lap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,11 +15,15 @@ void print_remaining_days(int month, int day, int year)
 	int yt;
 
 	yt = 2000;
-	if (((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0)) || year == yt)
+	if (((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0)) || (year == yt && day <= 59))
 	{
-		if (month >= 2 && day >= 60)
+		if (month >= 3 && day >= 60)
 		{
 			day++;
+		}
+		else if (month == 2 && day >60)
+		{
+			day--;
 		}
 
 		printf("Day of the year: %d\n", day);
@@ -27,7 +31,7 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month == 2 && day == 60)
+		if (month == 2 && day >= 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
