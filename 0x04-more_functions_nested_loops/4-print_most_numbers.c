191a8680efbb55e5fb39617c26ee961e5f5ee92c
dalexach <dalexach@gmail.com>
2019-06-27 14:48:40 +0000

    Print the numbers from 0 to 9 without 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* print_most_numbers - print the numbers from 0 to 9 without 2 & 4
+*
+*Return: Nothing
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
