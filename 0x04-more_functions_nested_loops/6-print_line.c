a4a2dab9a59ba687f3d65aa50ecb21edd727767c
dalexach <dalexach@gmail.com>
2019-06-27 15:05:05 +0000

    Drawing a straight line on the terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+* print_line - Draws a straight line
+*@n: number of times the character _ should be printed
+*Return: Nothing
+*/
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
