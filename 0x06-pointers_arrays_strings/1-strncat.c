2f548641bff17fc4c5ff9acc30b73717c577644a
dalexach <dalexach@gmail.com>
2019-07-04 17:05:49 +0000

    Other concatenation of two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* _strlen - returns the lenght of a string
+*@s: poiter of character
+*Return: the length of a string
+*/
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
+}
+
+/**
+* *_strncat - concatenates two strings
+*@dest:
+*@src:
+*@n:
+*Return: dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, len;
+
+	len = _strlen(dest);
+	for (i = 0; (i < n && src[i] != '\0'); i++)
+	{
+		dest[len + i] = src[i];
+	}
+	return (dest);
+}
