dc51812d0afc806348a549c723d17fcfdb9d436f
dalexach <dalexach@gmail.com>
2019-07-05 01:35:28 +0000

    Encoding in rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -8,20 +8,24 @@ char *rot13(char *c)
 {
 	int i;
 
-	for (i = 0; c[i] != '\0'; i++)
+	i = 0;
+	while (c[i] != '\0')
 	{
-		if ((c[i] >= 'a' && c[i] <= 'm') || (c[i] >= 'A' && c[i] <= 'M'))
+		while ((c[i] >= 'a' && c[i] <= 'z')
+			|| (c[i] >= 'A' && c[i] <= 'Z'))
 		{
-			c[i] = c[i] + 13;
-		}
-		else
-		{
-			while ((c[i] >= 'n' && c[i] <= 'z')
-				|| (c[i] >= 'N' && c[i] <= 'Z'))
+			if ((c[i] >= 'a' && c[i] <= 'm')
+				|| (c[i] >= 'A' && c[i] <= 'M'))
+			{
+				c[i] = c[i] + 13;
+			}
+			else
 			{
 				c[i] = c[i] - 13;
 			}
+			i++;
 		}
+		i++;
 	}
 	return (c);
 }
