e23bd609c5f1ab304315f7e10fac8575b443583f
dalexach <dalexach@gmail.com>
2019-07-05 00:00:02 +0000

    Encoding

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* *leet - encodes a string into 1337
+*@c: string received
+*Return: string changed
+*/
+char *leet(char *c)
+{
+	int i, j;
+	char a[] = "oOlLeEaAtT";
+	char n[] = "0011334477";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (c[i] == a[j])
+			{
+				c[i] = n[j];
+			}
+		}
+	}
+	return (c);
+}
