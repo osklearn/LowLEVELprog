75dc2c809941f5b0d21ea7938da7882958ce0758
dalexach <dalexach@gmail.com>
2019-07-05 01:24:53 +0000

    Encoding in rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+* *rot13 - encodes a string using rot13
+*@c: string to convert
+*Return: encoded string
+*/
+char *rot13(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if ((c[i] >= 'a' && c[i] <= 'm') || (c[i] >= 'A' && c[i] <= 'M'))
+		{
+			c[i] = c[i] + 13;
+		}
+		else
+		{
+			while ((c[i] >= 'n' && c[i] <= 'z')
+				|| (c[i] >= 'N' && c[i] <= 'Z'))
+			{
+				c[i] = c[i] - 13;
+			}
+		}
+	}
+	return (c);
+}
