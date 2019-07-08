345cdc4dd2de4d59ca08e3fae7f0c4f5c8b0fbff
dalexach <dalexach@gmail.com>
2019-07-08 15:19:40 +0000

    Memcpy - Copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* *_memcpy - copies memory area
+*@dest: destination to copy
+*@src: source pointer
+*@n: number of bytes to coppy
+*Return: dest (destination)
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
