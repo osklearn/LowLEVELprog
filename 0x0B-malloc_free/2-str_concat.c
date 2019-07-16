1bd0049864241a0a91e36e2b1f2bbe40dd27f045
dalexach <dalexach@gmail.com>
2019-07-16 17:34:05 +0000

    Concatenated

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,67 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* _strlen - returns the lenght of a string
+*@s: poiter of character
+*Return: the length of a string
+*/
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
+}
+
+/**
+* *str_concat - concatenates two strings
+*@s1: first string to concatenate
+*@s2: second string to concatenate
+*Return: the ponter to string concatenate or NULL
+*/
+char *str_concat(char *s1, char *s2)
+{
+	int l1, l2;
+	int i, j;
+	char *empty, *concat;
+
+	/**Calculating the length of the strings*/
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
+
+	/**lets make empty strings*/
+	empty = "";
+
+	/**comparing the enter strings*/
+	if (s1 == NULL)
+		s1 = empty;
+	if (s2 == NULL)
+		s2 = empty;
+
+	/**assign memory*/
+	/**increments l2 for the null character*/
+	l2++;
+	concat = malloc((l1 + l2) * sizeof(char));
+
+	/**verify if memory fails*/
+	if (concat == NULL)
+		return (NULL);
+
+	/**concatenate the strings*/
+	for (i = 0; i < l1; i++)
+	{
+		concat[i] = s1[i];
+	}
+
+	for (j = 0; j < l2; j++)
+	{
+		concat[i] = s2[j];
+		i++;
+	}
+
+	return (concat);
+}
