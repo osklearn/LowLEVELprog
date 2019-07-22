3e9d08c154ccacd343437e264f0ed98066398306
dalexach <dalexach@gmail.com>
2019-07-22 16:58:08 +0000

    dog structure actualized

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,11 +14,11 @@ struct dog
 };
 
 /**
-* my_dog_t - typedef for dog
+* dog_t - typedef for dog
 */
-typedef struct dog my_dog_t;
-void free_dog(my_dog_t *d);
-my_dog_t *new_dog(char *name, float age, char *owner);
+typedef struct dog dog_t;
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
