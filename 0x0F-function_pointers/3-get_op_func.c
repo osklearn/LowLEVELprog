6064879d47c11bcb5666eba4be5fb78b3cb010ee
dalexach <dalexach@gmail.com>
2019-07-24 21:01:22 +0000

    Function that redirects to the function to operate

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* get_op_func - select the correct operation
+*@s: operator
+*Return: a pointer to a function of the operation
+*/
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	while (i < 5)
+	{
+		if (*(ops[i]).op == *s && *(s + 1) == '\0')
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
