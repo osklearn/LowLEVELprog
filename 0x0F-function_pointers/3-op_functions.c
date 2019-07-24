d33ba555e05754d1d035fc35eec760acce6a5baf
dalexach <dalexach@gmail.com>
2019-07-24 20:10:45 +0000

    functions that opere the numbers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* op_add - function that sum 2 numbers
+*@a: first number
+*@b: second number
+*Return: the sum of the numbers
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* op_sub - function that substract 2 numbers
+*@a: first number
+*@b: second number
+*Return: the substract of the numbers
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - function that multiply 2 numbers
+*@a: first number
+*@b: second number
+*Return: the multiply of the numbers
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - function that divide 2 numbers
+*@a: first number
+*@b: second number
+*Return: the division of the numbers
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - function that calculate the remainder of the division of 2 numbers
+*@a: first number
+*@b: second number
+*Return: the remainder of the division of the numbers
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
