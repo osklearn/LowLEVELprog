40280de1daee78f3c980c2c496c6624c6da7f87d
dalexach <dalexach@gmail.com>
2019-07-24 21:48:33 +0000

    Main function for the calculator correct division and modulo

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
+	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == '0')
 	{
 		printf("Error\n");
 		exit(100);
