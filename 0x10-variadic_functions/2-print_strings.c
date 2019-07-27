fda86a94caa32fa261e1dde54d14d0bf5a486c24
dalexach <dalexach@gmail.com>
2019-07-27 17:13:29 +0000

    change order

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,10 +21,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		string = va_arg(strings, char *);
-		if (string != NULL)
-			printf("%s", string);
-		else
+		if (string == NULL)
 			printf("(nil)");
+		else
+			printf("%s", string);
 		if (i < (n - 1) && separator != '\0')
 			printf("%s", separator);
 	}
