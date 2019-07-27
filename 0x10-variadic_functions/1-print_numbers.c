4e3772733c0edef261bffb25eefdc554b06b0ab0
dalexach <dalexach@gmail.com>
2019-07-27 17:24:33 +0000

    Betty correction and delete number verify

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,9 +12,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list number;
 	unsigned int i;
 
-	/**if (n <= 0)
-		return;*/
-
 	va_start(number, n);
 
 	for (i = 0; i < n; i++)
