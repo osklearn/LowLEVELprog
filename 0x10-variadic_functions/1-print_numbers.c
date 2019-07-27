469c16a1aa679b0c01351ef64dd94009f51bb60a
dalexach <dalexach@gmail.com>
2019-07-27 17:22:26 +0000

    change conditions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,11 +19,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (separator == NULL)
-			printf("%d", va_arg(number, unsigned int));
-		else
-			printf("%d", va_arg(number, unsigned int));
-		if (i < (n - 1) && separator != '\0')
+		printf("%d", va_arg(number, unsigned int));
+		if (i < (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 
