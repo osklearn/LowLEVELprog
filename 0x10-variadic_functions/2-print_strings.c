060786d492d812f752d0f94744a13eea441ca925
dalexach <dalexach@gmail.com>
2019-07-27 17:26:12 +0000

    Fix the condition of number

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,9 +13,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *string;
 
-	if (n <= 0)
-		return;
-
 	va_start(strings, n);
 
 	for (i = 0; i < n; i++)
