e4eb68a1618cb6dc3b0fed0e7a65f3f672e3fe16
dalexach <dalexach@gmail.com>
2019-07-27 17:07:08 +0000

    corrected function

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,7 +13,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *string;
 
-	if (separator == NULL || n <= 0)
+	if (n <= 0)
 		return;
 
 	va_start(strings, n);
