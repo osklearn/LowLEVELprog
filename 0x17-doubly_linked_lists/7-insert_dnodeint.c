01a351426b89170dda939d7185b6bcfb38ea4f0f
dalexach <dalexach@gmail.com>
2019-09-15 03:32:51 +0000

    Verify if head is null and the index is different from 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,36 +13,35 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *temp, *temp2, *new;
-	unsigned int counter = 0;
+	unsigned int counter;
 
+	if (h == NULL && idx != 0)
+		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-
-	new->n = n, new->prev = NULL;
+	new->n = n;
+	new->prev = NULL;
 	if (*h == NULL)
 	{
 		if (idx > 0)
-			free(new); return (NULL);
+			free(new), return (NULL);
 		new->next = NULL, *h = new;
 		return (new);
 	}
-
 	if (idx == 0)
 	{
-		new->next = *h;
-		(*h)->prev = new;
+		new->next = *h, (*h)->prev = new;
 		*h = new;
 		return (new);
 	}
-
-	temp = *h;
-	while (temp != NULL && counter <= idx)
+	for (counter = 0, temp = *h; temp != NULL && counter <= idx;)
 	{
 		temp2 = temp->next;
 		if (counter == idx - 1)
 		{
-			temp->next = new, new->prev = temp;
+			temp->next = new;
+			new->prev = temp;
 			if (temp2 == NULL)
 				new->next = NULL;
 			else
