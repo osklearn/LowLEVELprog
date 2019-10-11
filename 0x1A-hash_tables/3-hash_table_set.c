fb4c5e20ccb067a57546c4b5f8e94ad888906779
dalexach <dalexach@gmail.com>
2019-10-11 17:00:46 +0000

    Adding the next header!

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -30,6 +30,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			head->value = strdup(value);
 			return (1);
 		}
+		head = head->next
 	}
 
 	new_node = malloc(sizeof(hash_node_t));
