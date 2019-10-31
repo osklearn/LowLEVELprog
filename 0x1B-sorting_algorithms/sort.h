e778d8629a27554b7e51860399c7c8def6863f90
dalexach <dalexach@gmail.com>
2019-10-31 03:34:46 +0000

    Upade header

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -21,7 +21,11 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void check(listint_t *temp, listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void lquick (int *array, size_t size, size_t start, size_t end);
+size_t partition(int *array, size_t size, size_t start, size_t pivot);
+void swap(int *i, int *j);
 
 #endif /**_SORT_H*/
