6b45999ce622054323aef6987307a9b5e9eeef2a
dalexach <dalexach@gmail.com>
2019-11-15 20:17:46 +0000

    Fixing PEP8 and conditions of the lenght of the grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,6 +4,7 @@ Function island_perimeter that returns the perimeter of the island
 described in grid
 """
 
+
 def island_perimeter(grid):
     """
     Function island_perimeter that calculates the perimeter on a grid
@@ -23,8 +24,8 @@ def island_perimeter(grid):
                     perimeter += 1
                 if y == 0 or grid[x][y - 1] == 0:
                     perimeter += 1
-                if x == len(grid) or grid[x + 1][y] == 0:
+                if x == len(grid) - 1 or grid[x + 1][y] == 0:
                     perimeter += 1
-                if y == len(grid[x]) or grid[x][y + 1] == 0:
+                if y == len(grid[x]) - 1 or grid[x][y + 1] == 0:
                     perimeter += 1
     return (perimeter)
