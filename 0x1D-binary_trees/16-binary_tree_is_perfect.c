89084b13e17465de6fc50d193fd8c42f32d2833c
dalexach <dalexach@gmail.com>
2019-12-04 21:33:23 +0000

    Function that checks if a binary tree is perfect with the equation

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -1,8 +1,40 @@
 #include "binary_trees.h"
+#include "9-binary_tree_height.c"
+#include "11-binary_tree_size.c"
 
 /**
- *  - Function 
- * @: 
- * Return: 
+ * _pow - Function to calculate the power of a number
+ * @x: number to power
+ * @n: number
+ * Return: x to the n
 */
 
+int _pow(int x, int n)
+{
+	int i, number = 1;
+
+	for (i = 0; i < n; i++)
+		number *= x;
+
+	return (number);
+}
+
+/**
+ * binary_tree_is_perfect - Function that checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree to check
+ * Return: 1 if the tree is perfect, otherwise 0
+*/
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int size, height, perfect;
+	if (tree == NULL)
+		return (0);
+	size = binary_tree_size(tree);
+	height = binary_tree_height(tree);
+	perfect = _pow(2, (height + 1)) - 1;
+
+	if (size == perfect)
+		return (1);
+	return (0);
+}
