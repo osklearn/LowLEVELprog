7d2562ed53c3a4d4c5c56f85d15f77d9fe48497c
dalexach <dalexach@gmail.com>
2019-12-04 20:10:56 +0000

    Function that calulate the leaves on a binary tree

--- 0x1D-binary_trees/12-binary_tree_leaves.c
+++ 0x1D-binary_trees/12-binary_tree_leaves.c
@@ -1,8 +1,23 @@
 #include "binary_trees.h"
 
 /**
- *  - Function 
- * @: 
- * Return: 
+ * binary_tree_leaves - Function that counts the leaves in a binary tree
+ * @tree: pointer to the root node of the tree to count the number of leaves
+ * Return: the number of leaves, otherwise 0
 */
 
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves, l_leaves, r_leaves;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	l_leaves = binary_tree_leaves(tree->left);
+	r_leaves = binary_tree_leaves(tree->right);
+
+	leaves = l_leaves + r_leaves;
+	return (leaves);
+}
