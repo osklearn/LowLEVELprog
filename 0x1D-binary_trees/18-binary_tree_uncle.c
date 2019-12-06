7a580b91a8f58cd069439fab5d755d34caf64a3f
dalexach <dalexach@gmail.com>
2019-12-06 04:34:00 +0000

    adding one parent

--- 0x1D-binary_trees/18-binary_tree_uncle.c
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -16,7 +16,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node->parent->parent->right != NULL &&
 		node->parent->parent->left != NULL)
 	{
-		if (node->parent->parent->right != node)
+		if (node->parent->parent->right != node->parent)
 			return (node->parent->parent->right);
 		else
 			return (node->parent->parent->left);
