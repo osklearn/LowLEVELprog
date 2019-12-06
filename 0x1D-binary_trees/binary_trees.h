780ec4ff47adca9af97f02a25eb2e740fef59999
dalexach <dalexach@gmail.com>
2019-12-06 04:44:46 +0000

    Update the last function for task 19

--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -69,5 +69,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 /** task 18*/
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+/**task 19*/
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+const binary_tree_t *second);
 
 #endif /* _BINARY_TREES_H_ */
