486aefb0ac1cfa9593d4a61ac262315b225bdd65
dalexach <dalexach@gmail.com>
2020-01-14 16:05:22 +0000

    Uploading the header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef _SEARCH_ALGORITHMS_H_
+#define _SEARCH_ALGORITHMS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/** Functions */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /** _SEARCH_ALGORITHMS_H_ */
